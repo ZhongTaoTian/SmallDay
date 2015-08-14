89c9ee748d9a14fa705812240796feb07b3b3e85
MacBook <macbook@tianzhongtao.local>
2015-08-14 14:50:50 +0800

    Initial Commit

---
+++ SmallDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmallDay
+//
+//  Created by MacBook on 15/8/14.
+//  Copyright (c) 2015年 维尼的小熊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
