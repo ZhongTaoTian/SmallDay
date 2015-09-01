46662d7bd1173f4b5ea2355f6a7605b08a640bf6
MacBook <macbook@tianzhongtao.local>
2015-09-02 01:32:03 +0800

    更新了一下CocoaPods

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MJRefresh
+#define COCOAPODS_POD_AVAILABLE_MJRefresh
+#define COCOAPODS_VERSION_MAJOR_MJRefresh 2
+#define COCOAPODS_VERSION_MINOR_MJRefresh 4
+#define COCOAPODS_VERSION_PATCH_MJRefresh 7
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 1
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 1
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 3
+
--- SmallDay/Classes/UI/Other/SmallDay-Bridging-Header.h
+++ SmallDay/Classes/UI/Other/SmallDay-Bridging-Header.h
@@ -2,7 +2,6 @@
 #import <CommonCrypto/CommonCrypto.h>
 #import <SVProgressHUD/SVProgressHUD.h>
 #import <MJRefresh.h>
-#import <UIImageView+WebCache.h>
 #import "UMSocial.h"
 #import "UMSocialSinaHandler.h"
 #import "UMSocialWechatHandler.h"
